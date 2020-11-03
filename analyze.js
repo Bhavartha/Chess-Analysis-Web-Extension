e97c786b7169c347bc3fd7184b86ac1c885ad700
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-11-04 00:28:23 +0530

    function to modify dom

--- analyze.js
+++ analyze.js
@@ -1,1 +1,29 @@
-console.log("Extension Running");
+let timer;
+
+function replaceButton() {
+
+    // Get Analysis Button
+    const btn_original = document.querySelector(".quick-analysis-buttons > .ui_v5-button-basic")
+
+    // If the button doesnt exist then exit the function
+    if (typeof (btn_original) == 'undefined' || btn_original == null) {
+        return
+    }
+
+    // Clone the Analysis Button 
+    // Note: It doesnt clone onclick events
+    const btn_custom = btn_original.cloneNode(true);
+
+    // Get the parent node of the Analysis button
+    const parent = btn_original.parentNode
+    btn_original.replaceWith(btn_custom)
+
+    // Modify the click event on btn so that when we click it opens the game in lichess
+    btn_custom.onclick = function () {
+        console.log("RUNNIN");
+    };
+
+    window.clearInterval(timer)
+}
+
+timer = window.setInterval(function () { replaceButton() }, 2000);
